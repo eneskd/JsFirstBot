7c9c89655ae4ea77b4ea3f177aee157f28a94b7f
Mirathi <>
2021-05-13 16:24:52 +0000

    replit implementation

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+const token = process.env['token']
+
 
 client.on('ready', () => {console.log('The client is ready!')})
 
-client.login(config.token)
+client.login(token)
 
 
