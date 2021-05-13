e1381f04559a060d8cdbab70ae2d2f5ea423479b
Mirathi <>
2021-05-13 17:12:30 +0000

    plaaceholder error

---
+++ command.js
@@ -0,0 +1,26 @@
+const { prefix } = require('./config.json')
+
+
+
+
+module.exports = (client, aliases, callback) => {
+	if (typeof aliases === 'string'){
+		aliases = [aliases]
+	}
+
+	client.on('message', message => {
+		const {content} = message
+		
+		aliases.forEach(alias => {
+			const command = '${prefix}${alias}'
+			console.log(command)
+
+			if (content.startsWith('${command} ') || content === command){
+				console.log('Running the command ${command}')
+				callback(message)
+		}})
+	
+		
+		})
+		
+	}
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const token = process.env['token']
+const TOKEN = process.env['token']
+const config = require('./config.json')
+const command = require('./command')
 
 
-client.on('ready', () => {console.log('The client is ready!')})
+client.on('ready', () => {
+	console.log('The client is ready!')
 
-client.login(token)
+	command(client, 'ping', message => {
+		message.channel.send('Pong!')
+	} )
+
+
+})
+
+client.login(TOKEN)
 
 
